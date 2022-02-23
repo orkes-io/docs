60475c96f0b59cfb09b075ae6dff61a0eb1b20bd
James Stuart Milne <jstuartmilne@gmail.com>
2022-02-23 11:30:16 -0300

    feat: added common containers home page

---
+++ src/components/PaperContainer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./PaperContainer.module.css";
+
+export const PaperContainer = ({ children, className = "" }) => {
+  return <div className={`${styles.paper} ${className}`}>{children}</div>;
+};
+
+export const ColumnRenderer = ({ columns, spaceEvenly = false }) => {
+  const columnAmount = Math.round(12 / columns.length);
+  return (
+    <div className={`row ${spaceEvenly ? styles.spaceEvenly : ""}`}>
+      {columns.map((elem, idx) => (
+        <div key={`col-${idx}`} className={`col col--${columnAmount}`}>
+          {elem}
+        </div>
+      ))}
+    </div>
+  );
+};
+export const PaperColumnRenderer = ({ columns, spaceEvenly = false }) => {
+  const columnAmount = Math.round(12 / columns.length);
+  return (
+    <PaperContainer className={`container padding--lg`}>
+      <div className={`row ${spaceEvenly ? styles.spaceEvenly : ""}`}>
+        {columns.map((elem, idx) => (
+          <div key={`col-${idx}`} className={`col col--${columnAmount}`}>
+            {elem}
+          </div>
+        ))}
+      </div>
+    </PaperContainer>
+  );
+};
+
+export const InfoPaper = ({ title, infoColumn1, infoColumn2 }) => (
+  <PaperContainer className={`container padding--lg`}>
+    <div className="row row--no-gutters">
+      <h4>{title}</h4>
+    </div>
+    <ColumnRenderer columns={[infoColumn1, infoColumn2]} />
+  </PaperContainer>
+);
