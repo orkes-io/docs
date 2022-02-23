0e3c58756e5a74c698249da9087c369c569c154f
James Stuart Milne <jstuartmilne@gmail.com>
2022-02-23 17:56:28 -0300

    feat: added single step for small boxes

--- src/components/SingleStep.js
+++ src/components/SingleStep.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./SingleStep.module.css";
+import Link from "@docusaurus/Link";
+import { PaperContainer } from "./PaperContainer";
 
-export const SingleStep = ({ title, description }) => (
-  <div className={`container padding--md ${styles.singleStep}`}>
-    <div className="row padding-horiz--md">
-      <h4 className={styles.title}>{title}</h4>
-    </div>
-    <div className="row padding-horiz--md">
-      <p className="description">{description}</p>
-    </div>
-  </div>
+export const SingleStep = ({ title, description, to }) => (
+  <PaperContainer className={styles.singleStep}>
+    <Link to={to} className="noStyleLink">
+      <div className={`container padding--md`}>
+        <div className="row padding-horiz--md">
+          <h4 className={styles.title}>{title}</h4>
+        </div>
+        <div className="row padding-horiz--md">
+          <p className="description">{description}</p>
+        </div>
+      </div>
+    </Link>
+  </PaperContainer>
 );
