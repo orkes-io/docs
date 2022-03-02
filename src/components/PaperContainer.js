e02bb3787a85ba3bfcff2fb6f870060b1994eda3
James Stuart Milne <jstuartmilne@gmail.com>
2022-03-02 18:43:01 -0300

    feat: added additional container for two columns two titles

--- src/components/PaperContainer.js
+++ src/components/PaperContainer.js
@@ -17,13 +17,21 @@ export const ColumnRenderer = ({ columns, spaceEvenly = false }) => {
     </div>
   );
 };
-export const PaperColumnRenderer = ({ columns, spaceEvenly = false }) => {
+
+export const PaperColumnRenderer = ({
+  columns,
+  spaceEvenly = false,
+  colClassName = "",
+}) => {
   const columnAmount = Math.round(12 / columns.length);
   return (
     <PaperContainer className={`container padding--lg`}>
       <div className={`row ${spaceEvenly ? styles.spaceEvenly : ""}`}>
         {columns.map((elem, idx) => (
-          <div key={`col-${idx}`} className={`col col--${columnAmount}`}>
+          <div
+            key={`col-${idx}`}
+            className={`col col--${columnAmount} ${colClassName}`}
+          >
             {elem}
           </div>
         ))}
@@ -39,5 +47,36 @@ export const InfoPaper = ({ title, infoColumn1, infoColumn2, children }) => (
     </div>
     <ColumnRenderer columns={[infoColumn1, infoColumn2]} />
     {children}
+  </PaperContainer>
+);
+
+export const DoubleInfoPaper = ({
+  title1,
+  title2,
+  infoColumn1,
+  infoColumn2,
+}) => (
+  <PaperContainer className={`padding--md padding-top--lg`}>
+    <div className="container">
+      <div className="row">
+        <div className="col col--6">
+          <div className="container">
+            <div className="row padding-left--md">
+              <h4>{title1}</h4>
+            </div>
+            <div className="row">{infoColumn1}</div>
+          </div>
+        </div>
+
+        <div className="col col--6">
+          <div className="container">
+            <div className="row padding-left--md">
+              <h4>{title2}</h4>
+            </div>
+            <div className="row">{infoColumn2}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </PaperContainer>
 );
