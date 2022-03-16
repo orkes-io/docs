d62c6b772de0c5ff76c02e0d017dc5dfc1a1bfc6
Pablo Cuadrado <pablocuadrado@gmail.com>
2022-03-16 10:38:30 -0300

    Set Dark Theme as default.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,9 @@ const config = {
     themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
+            colorMode: {
+                defaultMode: 'dark',
+            },
             navbar: {
                 title: 'Conductor Docs',
                 logo: {
