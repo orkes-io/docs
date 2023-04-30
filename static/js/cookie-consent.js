fb1117bb69fbcd05e43aaa50d2d86225acbfbbda
boney9 <boneysekh@gmail.com>
2023-04-30 13:20:07 -0700

    Adding canonical link to the head

--- static/js/cookie-consent.js
+++ static/js/cookie-consent.js
@@ -1,4 +1,4 @@
-var script = document.createElement('script');
+const script = document.createElement('script');
 script.src = "https://app.termly.io/embed.min.js";
 
 script.setAttribute("data-auto-block", "on");
@@ -6,7 +6,7 @@ script.setAttribute("data-website-uuid", "366ddc52-323f-4589-afeb-39d61e0cc6e9")
 
 document.head.appendChild(script);
 
-var canonicalLink = document.createElement('link');
+const canonicalLink = document.createElement('link');
 canonicalLink.setAttribute("rel","canonical");
 canonicalLink.setAttribute("href", `https://orkes.io${window.location.pathname}${window.location.search}`);
 
