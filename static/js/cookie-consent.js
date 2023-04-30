57bfe1382da73a34ff74a49cc11d9ca379c1b976
boney9 <boneysekh@gmail.com>
2023-04-30 13:19:26 -0700

    Adding canonical link to the head

--- static/js/cookie-consent.js
+++ static/js/cookie-consent.js
@@ -5,3 +5,9 @@ script.setAttribute("data-auto-block", "on");
 script.setAttribute("data-website-uuid", "366ddc52-323f-4589-afeb-39d61e0cc6e9");
 
 document.head.appendChild(script);
+
+var canonicalLink = document.createElement('link');
+canonicalLink.setAttribute("rel","canonical");
+canonicalLink.setAttribute("href", `https://orkes.io${window.location.pathname}${window.location.search}`);
+
+document.head.appendChild(canonicalLink);
