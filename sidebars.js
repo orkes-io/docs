9973dfa91dd6781dade010bca138e6cab28c9524
RizaFarheen <rizafarheen@gmail.com>
2025-08-01 11:19:48 +0400

    updates

--- sidebars.js
+++ sidebars.js
@@ -612,7 +612,6 @@ const sidebars = {
                             'reference-docs/system-tasks/update-secret',
                             'reference-docs/system-tasks/get-signed-jwt',
                             'reference-docs/system-tasks/update-task',
-                            'reference-docs/system-tasks/grpc',
                         {
                             type: 'category',
                             label: 'Alerting Tasks',
