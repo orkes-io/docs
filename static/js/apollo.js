305a936badf411832eedc613cdefc69823e7b6c2
najeebkp <najeebthangalkp@gmail.com>
2026-02-12 10:58:02 +0530

    Remove tracking services: Apollo, Termly, and Vimeo player

--- static/js/apollo.js
+++
@@ -1,4 +0,0 @@
-function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
-o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-o.onload=function(){window.trackingFunctions.onLoad({appId:"6618375bc3f47001afd0877d"})},
-document.head.appendChild(o)}initApollo();
