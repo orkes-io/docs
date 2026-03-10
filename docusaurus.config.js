0b025e08919c532ff48de330cb0b99d393e3e9d0
RizaFarheen <rizafarheen@gmail.com>
2026-03-10 15:48:59 +0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,6 +112,7 @@ const config = {
             to: "/content/",
             position: "left",
             label: "Home",
+            exact: true,
           },
           {
             to: "/content/quickstarts",
